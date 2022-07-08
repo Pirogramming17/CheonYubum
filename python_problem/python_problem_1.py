24cd9ca5f495cac4f3734a0abfc2227512a8ab8e
YuBeom Chun <elshadaiyb@gmail.com>
2022-07-09 04:53:17 +0900

    1-5

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,6 +1,8 @@
 num = 0
+time = 0
 
 while True:
+    time += 1
     try:
         x = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
     except:
@@ -9,8 +11,9 @@ while True:
         if x < 1 or x > 3:
             print('1,2,3 중 하나를 입력하세요')
         else:
-            break
-
-for i in range(x):
-    num += 1
-    print('Player : ', num)
+            for i in range(x):
+              num += 1
+              if time % 2 != 0:
+                print('Player A: ', num)
+              else:
+                print('Player B: ', num)
