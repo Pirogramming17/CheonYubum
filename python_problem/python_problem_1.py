504f27f11e03fe3073b1720138405a4ab8184637
YuBeom Chun <elshadaiyb@gmail.com>
2022-07-09 05:08:30 +0900

    unit 7

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,10 +1,11 @@
 num = 0
 time = 0
+turn = 0
 
 while True:
     time += 1
     try:
-        x = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
+        x = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : '))
     except:
         print('정수를 입력하세요')
     else:
@@ -14,10 +15,14 @@ while True:
             for i in range(x):
               num += 1
               if time % 2 != 0:
-                print('Player A: ', num)
+                print('PlayerA : ', num)
+                turn = 1
                 if num == 31:
+                  print('PlayerB win!')
                   break
               else:
-                print('Player B: ', num)
+                print('PlayerB : ', num)
+                turn = 2
                 if num == 31:
+                  print('PlayerA win!')
                   break
